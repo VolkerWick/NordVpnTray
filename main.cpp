2b808dfa596a6b8684b12fd5e97a01f4a2325ce4
VolkerWick <volker.wick+github@gmail.com>
2020-05-23 16:46:51 +0200

    Allow single instance to be run

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    return a.exec();
+    QSharedMemory sharedMemory;
+    sharedMemory.setKey("89e58d62-3d2d-4ef6-a849-cba25d6064ec");
+
+    int result = -1;
+
+    if (sharedMemory.create(1)) {
+        QApplication a(argc, argv);
+        MainWindow w;
+        result = a.exec();
+    }
+
+    return result;
 }
