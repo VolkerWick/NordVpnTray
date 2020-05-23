3b224522c8dafed81eb959ef50c554ad6902d2e6
VolkerWick <volker.wick+github@gmail.com>
2020-05-23 17:23:02 +0200

    Revert "Allow single instance to be run"
    This reverts commit 2b808dfa596a6b8684b12fd5e97a01f4a2325ce4.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,10 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
-    QSharedMemory sharedMemory;
-    sharedMemory.setKey("89e58d62-3d2d-4ef6-a849-cba25d6064ec");
-
-    int result = -1;
-
-    if (sharedMemory.create(1)) {
-        QApplication a(argc, argv);
-        MainWindow w;
-        result = a.exec();
-    }
-
-    return result;
+    QApplication a(argc, argv);
+    MainWindow w;
+    return a.exec();
 }
